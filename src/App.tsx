ec1cd21e290d03f5f5696ce4795b67007c748992
Raymond Abiola <raymondintell@gmail.com>
2025-10-24 17:53:26 +0100

    added code for react app with hooks and also added styling using css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import questions from "./questions.json";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswer = (isCorrect: boolean) => {
+    if (isCorrect) setScore(score + 1);
+
+    const next = currentQuestion + 1;
+    if (next < questions.length) {
+      setCurrentQuestion(next);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <div className="question-section">
+          <h2>
+            Question {currentQuestion + 1} / {questions.length}
+          </h2>
+          <h3>{questions[currentQuestion].question}</h3>
+          <div className="answer-section">
+            {questions[currentQuestion].answers.map((ans, index) => (
+              <button key={index} onClick={() => handleAnswer(ans.isCorrect)}>
+                {ans.text}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
